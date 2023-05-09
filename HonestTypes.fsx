6ed50d001a3a10378fbf70e54936b4b958f6b801
Darran Hayes <2190584+darranhayes@users.noreply.github.com>
2023-05-09 12:49:13 +0100

    Change result ce to validation ce to return list of domain errors

--- HonestTypes.fsx
+++ HonestTypes.fsx
@@ -1,5 +1,6 @@
 (*
     https://demystifyfp.gitbook.io/fstoolkit-errorhandling/
+    https://www.compositional-it.com/news-blog/validation-with-f-5-and-fstoolkit/
 *)
 
 #r "nuget: FsToolkit.ErrorHandling"
@@ -11,15 +12,31 @@ type DomainError =
 | Null
 | Whitespace of string
 | Empty
+| MinLength of int
 
 let notNull<'a when 'a : null> (s: 'a) =
-    if isNull s then Error Null else Ok s
+    if isNull s then
+        Error Null
+    else
+        Ok s
 
 let notWhitespace (s: string) =
-    if not (isNull s) && String.IsNullOrWhiteSpace s then Error (Whitespace s) else Ok s
+    if not (isNull s) && String.IsNullOrWhiteSpace s then
+        Error (Whitespace s)
+    else
+        Ok s
 
 let notEmpty (s: string) =
-    if not (isNull s) && String.IsNullOrEmpty s then Error Empty else Ok s
+    if not (isNull s) && String.IsNullOrEmpty s then
+        Error Empty
+    else
+        Ok s
+
+let minChars (length: int) (s: string) =
+    if s.Length < length then
+        Error (MinLength length)
+    else
+        Ok s
 
 type AccountId =
     private | AccountId of string with
@@ -27,14 +44,14 @@ type AccountId =
         let (AccountId accountId) = this
         accountId
     static member Parse (accountId: string) =
-        result {
-            let! notNull = notNull accountId
-            let! notEmpty = notEmpty notNull
-            let! notWhitespace = notWhitespace notEmpty
-            return AccountId notWhitespace
+        validation {
+            let! _ = notNull accountId
+            and! _ = notEmpty accountId
+            and! _ = notWhitespace accountId
+            and! _ = minChars 3 accountId
+            return accountId
         }
 
-let result =
-    match AccountId.Parse ("123") with
-    | Ok id -> id.Value
-    | Error reason -> string reason
+match AccountId.Parse ("  f ") with
+| Ok id -> id
+| Error reason -> string reason
