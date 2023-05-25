66f28cbe20b2836ea654a7705bdd406270afa56b
Darran Hayes <2190584+darranhayes@users.noreply.github.com>
2023-05-25 14:32:27 +0100

    Add struct records to memory graph example

--- BoxedUnboxedMemoryLayouts.fsx
+++ BoxedUnboxedMemoryLayouts.fsx
@@ -1,47 +1,54 @@
-// https://sharplab.io/#v2:EYLgtghglgdgNAGxAMwM5wC4gE4Fd4AmIA1AD4D05ABAEID2AHgKYFUEQYRVSpXZPIm/GAGMmvYAE8qABzqwMQ3hBitYBKPxEYodGAFgAUJSoBVGMEYs2HLjyoi9OgOa46uVEaMnzl5qy06bFYAWgcnKFd3T0MMSRkmKgAFDCoAXioAbyMqXKoGECpkBDoOHLzJQuLSjCMAXy9janp/PiZHYKowpggRAAtk1Pt+QWExVj6hJiM4hKoAN3aMIPSs8tygyNhClPW2TSXdGB3awwbDb2arULbRplFxIwQmVLBcTmBnhYAlVcyqTbOWB/fKrACMADoAAwAbio0gyACZoVQ6nCNFodHoQQxwVDoXCEVREfioajUUYAJIwVAJbQQgCyTDAQUkAHFsBAZAMAGTzb6NEwtaxhEZTB4xZ6vd4QT6JeYAFVWAAoqMrIVC4MToQBKLXq0lakm6qg6qk0ukYRnM1kcrm8xWCq6tMIAQWwnOkdGQt3FYl4y0GbQ6BElLyobw+X3mrt+GQA2qQsqCMhrCatkWS0cncanSemkaS6lRSABdc20pbWlnYdmc7lUPmxp20a5dKjuz0An1isbiKiBjC4GTPMPSqPy11KhNJ9XQo26uEG+fE0k6kvlwzUyv0pk1uv2xsxhUt3xtt0eiCSZT8cIwFxuDxFFYybBQSA6RYLCAIXCPQxShGMpygsrqrImVBptqsKQfmq4omWFaWtWtr1g6rqnhYbaBJ0YSOPekSPjEsyJCIizaCsGTZIYeQAm+QIwAAGlUJRlDReSArAACaLE1HsGKHHozFFKxpy0QJ2hHDxIl8WcjSAZGsrRgAwnGyacUx4IEnRWwwFxGbaRJWKaXmhkHJJej6YWKLnNuyF7qhh58qpQA=
+// https://sharplab.io/#v2:EYLgtghglgdgNAGxAMwM5wC4gE4Fd4AmIA1AD4D05ABABYYYAOqIlqNE2DCEwAdFAHtyAYgBuAJhABRAJoAZAOYYFNBQgUEFAOQCCAcQAeOgLIB3YwFZTAYQAsCqbYBiBWzrABJHQEYdAEVtrUx0AVR0ABQAVMKkdAEUFcJkdAFMdBgUADTiAJQAhGgAJCBoAK1M80r85MAMMEIJrAHk5bFKAa2AAY0jrKS6ANmCAKQsABn927wUZFwB1HQBlUqgckOHS63aDJrGMGBzSsB0MKXDjGQEdLu8DLS728ULIvNRTYbBTbwBpBAEBnROEIyWYyABqwAQtj8izmXXBpgAHKIwdYBABHfQeAQALT8VxkcTyFkB7R0eSJw3JZJCcR0OSaVXaHjkDFEc28iJ0QXMkTBcTicwQcXEAxCYyk1j0Oli+IAXt8FCK/CZwVIxuIpItvjoBHIwl05qYPJFIhAAbq5lIgd4mnS0oUJuEFHlvk0XSlvqUBDBcKU5BYDDipAYEF0/Ao5SkAMyaPweOZOR46GghPSmBilSwKVAIVSiMAYZClDAATgEBCOXWw2C6hQYORgCCYrPaeWSUhyFmsOlMKQAnnKPKY5CCIMg5cg/DlcHIIB5o6YpMMZnprHEMBTkIUdOjTBZ42MUgodDiQlJcHkpKgoHMBR52jRUHLFlBjHEPGCdDpxOiCHkug8Yxhi6HRWRCPw8nRHIcQlf4cTkb5bDmOUmnGfwUlKcQBztaxrBkcITkiOUQhycR6AEUwvx0URsH7UwBAgHJFhxINvhxcIaAEXB9xxHIXAAdjBHJsG+Jw/zpbw9CcADwmjYxo3EXAaDyAh6I8CwBmjSIaAQLQ9CkftcC6fscXIYFCiaCDxBCEIPDREJiFQTIZGsMFFhMftbC0Uxvg/PRvGsQooAMGRTFMGgdAgLo5URTIaB7Pw5nRFJTC6Uw4lQJxUB0QougEPIFHEbALCgEzxFEARbEindPzmDxxGsaMYGGAQBGsBhvEKOk9FsIk7C6PQQggCA5WMOxMigUo5gMQpyDJYZFkiBgMEKAYZFQYZsGQORrEiGUZAME8VR0aNur0BQwBPSIPFUvJcCgPIUjCsAZBgCwwBgOJwlsMBsGIfQlnbJwxiaPIBlif8xiBOQdDGORENQcI5TGBRrFKCxwgYAZSzycJsDmUscUWdo9EKawIAwdEMBoHFUHaVamkKOZsFsLgIC0BhxAYKA5X7JovrlbBERSIVYLyAxkAELRsBkUopDoPQaYMKAPDiRZFjBAxMjZsAwGABQcWwdFQvRRY9DmYZTAgGQaGAHhuN9DAcikBAxmQAZjDAfsoG5iwdKiqBFjAcR9oIGAfT8UtUFwcR+0t0xsGGQpKpyyJ0X7AgdDlGAmmsAZ2hjmQBjwXBvGgJoPDQ4xCkvWwIEKeynykAQnAsTIYFsDBbFKLRjAMWxRAQYgBIGG2aGjCxyBoLinGGBgnqkKjTGjCA9C6MF+ycchsBoMApAhGhiEKBB2hyAT6OgvQ5XabJyH7EJgH7MAQmalJcDBQp2mIZwPi0L8wTGEWIUJUvVgA6AQH4KArk5QIB0MgMAAg9DARyBgDSYRNAKH5DoDwAxbDfBSKgeuegtApEyFguIDBrCZC0FAXAHgLwEHIKYTIBBhjkHiD9dE4Dvxgk2DkYI3x9rDC3jiREX1vzokvJBDwco445GsN4ASOJkApAMAJOGYAUhygEuEAGUhGBgm8KYfYqA146DmKUXAehoz3BoE4UQfhrBdGGLfREyAII4gFDoAAsAAKH8ZQKgeQBAGBSAQKgBBKYQCoKgDAeAugYFwNgQhVAOApCoCk1RKSYBdHCVQJ+VAGACFgBgFI2AqCwAIFAFJiTBAwDSTACJ6SqARxgAAWnyjADAUAFC4G4qgSpDStGILooE6gIQYDAFCfkqJGAYlQEGV0npfSBn+P8cIKAyAqDNGMOEFkUg/BUCCVLCpuBUAZNgLQegTAWDkDYBwdmkJ+ACH8QgFIGAhmoAYCkRJxgUijP7HobAEAGA0CoAACgMCAAp/YskAB4AD6AA+AAlFQAAvP4qgOKqAeBgN835GBeD/MBcC0F4KABkBgNkpAQBct5Hyvk/L+QCgQdFyVgshdC2FCKUXoqxX43FRTsClOQA0gARBTGAEdPnVO+dwfsVARnsqVQoEFXKrkEAEPsJlaAoAStpU0rZ6y/FBMWPE4ynzansoIIMlp+BplhIIP4gA2vCi1CSMDIoALr+IwP2H5VBwglO6Z6q1mKqAAG9sW4p5cgP4lNY04v7DChNjEMD+IAL6mvNZaxJmTfm2vtSkqgjqZkur8e68NiTfX+sDRksERL2U1s+Ri6NyaqDst6bAGFIbSmts7dU2pPSfR9tDRgQdfic0BLNdQVthb8rYDtWk0t5bnVwDSQgP4XRKazJqUShASqfSxPmQ8LtFTYAIFgBk0wUAaZXIgLKmg5TInRPaXEr1ySUibvyjzfJhTRAQAQLgFIjLPlgFwPMyEGTRCLByJGqNF6e0NPbUhgwkbvC8DGAAbioEq9t4hsNUCzXh4dRL6mIaoBh9t3gxjYbwwRqg4h6NjBIyR/xsBCWsrJRqqlcGci5uoDkIty6S0ZKdeE+tQb+3dJE0uiJaHO3xsTZmoVuLU1UHTUm6dQmqDyeLauiTFbpONubdgAzy7EOdu7QoXtwaJ2WcrcK8jdSx0OdKU57NemnPiYKRWzdwHrb9ntdugQu6ykRJPS+0FjSIkwBSKIV9V6b1UDvQ+hpT6dUvoqXMiAH781JJSb+gQ/6ImFKyeUlIuSwN+PeRBqDPB3lUFEAhtDyG7OoejdRzDDH8ORqI2x0jkSD1ua6+hzDrHGMDdY+xmdXGWUYFJaqzl/HBOzvq8qxrMGWtkUja1/xc4YB9IgAoFI+MoBgHvVAJLqBeCcRC2VYDUgpHAfvUq1ru3xD+NIMikVYrJWfcpV9ozsSIBaK7cAUoRKYUAFJgCGtnUEyIuAuCpJaZJytm3IPQea6ISIkaIWQqw2MTdg3UWbohXR7DZPWPotRZxgli3lscr41QSl+P/FAA===
 
-// Boxed data is references by pointers and indirection
+// Boxed data structures are referenced by pointer indirection and are non-contiguous in memory
 // Unboxed data is contiguous
 
-// Unboxed record - contiguous
-type Pt = {
+#if COMPILED // for use in https://sharplabl.io
+let inspectMemoryGraph (x: byref<_>) =
+    Inspect.MemoryGraph &x
+#else
+let inspectMemoryGraph (x: byref<_>) =
+    printfn "Cannot display memory graph in dotnet fsi"
+#endif
+
+// Struct records are unboxed
+[<Struct>]
+type PointStruct = {
     x: float
     y: float
 }
 
-// Boxed record - each Pt is referenced here
-type vector = {
-    origin: Pt
-    direction: Pt
+// Struct records are unboxed
+[<Struct>]
+type VectorStruct = {
+    origin: PointStruct
+    direction: PointStruct
 }
 
-// Boxed - references to Pt records
+// Struct records are unboxed, allocated directly on stack or inline within another data-structure, copied by value
+let mutable vSR = { origin = { x = 1.0; y = 2.0 }; direction = { x = 10.0; y = 20.0 } }
+inspectMemoryGraph &vSR
+
+// Records are boxed
+type PointRecord = {
+    x: float
+    y: float
+}
+
+// Records are boxed
+type VectorRecord = {
+    origin: PointRecord
+    direction: PointRecord
+}
+
+// Records are boxed, always allocated on heap and never inline within another data-structure, copied by reference
 let mutable vR = { origin = { x = 1.0; y = 2.0 }; direction = { x = 10.0; y = 20.0 } }
-// Inspect.MemoryGraph &vR
+inspectMemoryGraph &vR
 
-// Boxed - references to tuples
+let mutable vR2 = vR
+LanguagePrimitives.PhysicalEquality vR vR2
+|> printfn "vR & vR2 are same object: %b"
+
+// Tuples are boxed
 let mutable vT = ( (1.0, 2.0), (10.0, 20.0) )
-// Inspect.MemoryGraph &vT
-
-// Boxed-ish - Contiguous Array of references to Pt records
-let mutable vAR = [| { x = 1.0; y = 2.0 }; { x = 10.0; y = 20.0} |]
-// Inspect.MemoryGraph &vAR
-
-// Boxed-ish - Contiguous Array of references to tuples
-let mutable vAT = [| (1.0, 2.0); (10.0, 20.0) |]
-// Inspect.MemoryGraph &vAT
-
-// Unboxed - Contiguous array of floats, no references
-let mutable vA = [| 1.0; 2.0; 10.0; 20.0 |]
-// Inspect.MemoryGraph &vA
-
-// Unboxed record - Contiguous
-type cvector = {
-    originX: float
-    originY: float
-    directionX: float
-    directionY: float
-}
-
-let mutable vCR = { originX = 1.0; originY = 2.0; directionX = 10.0; directionY = 20.0 }
-// Inspect.MemoryGraph &vCR
+inspectMemoryGraph &vT
