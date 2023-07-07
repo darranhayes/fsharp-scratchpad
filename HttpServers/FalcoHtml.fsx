372419e73600ea2d052fe5e3e31629714a1b50c5
Darran Hayes <2190584+darranhayes@users.noreply.github.com>
2023-07-07 15:40:19 +0100

    Add Falco API example

---
+++ HttpServers/FalcoHtml.fsx
@@ -0,0 +1,21 @@
+(*
+    https://jkone27-3876.medium.com/net-script-apis-92b3414365f5
+
+    To add AspNetCode dependencies to be consumed by scripts:
+        curl https://gist.githubusercontent.com/jkone27/e3f9ca1dbba69c602a3eaa3563ab2bdb/raw/c720d21208410c93fd72ec3dcef556ade065ecc4/generateAspnetcore.fsx > generate-sdk-references.fsx
+        dotnet fsi generateAspnetcore.fsx # creates framework fsx files in runtime-scripts/
+        echo "runtime-scripts/" >> .gitignore
+*)
+
+#load "../runtime-scripts/Microsoft.AspNetCore.App-7.0.7.fsx"
+#r "nuget: Falco"
+
+open Falco
+open Falco.Routing
+open Falco.HostBuilder
+
+webHost [| |] {
+    endpoints [
+        get "/" (Response.ofPlainText "Hello World")
+    ]
+}
